ddea5527c71ffa05f29c475303e7a83ce095d90c
igoriusha <visfin@yandex.ru>
2025-06-16 18:25:07 +0300

    Commit 202506161824 add tests.py

--- perevalapi/perevalapp/tests.py
+++ perevalapi/perevalapp/tests.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import PerevalAdded, Author, PerevalImage, Coords
 
-# Create your tests here.
+
+class PerevalAddedTest(TestCase):
+    def test_create_object(self):
+        user = User.objects.create(username='testuser', email='testuser@mail.ru')
+        author = Author.objects.create(user=user)
+        pereval = PerevalAdded.objects.create(
+            beautyTitle = 'Тест1',
+            title = 'Название1',
+            other_titles = 'Другое название1',
+            connect = 'Соединение',
+            user = author,
+            level_winter = 'B2',
+            level_summer = 'B1',
+            level_autumn = 'A2',
+            level_spring = ''
+        )
+        Coords.objects.create(pereval=pereval,
+                              latitude=12.2585,
+                              longitude=65.2569,
+                              height=800
+                              )
+        PerevalImage.objects.create(pereval=pereval,
+                                    images=SimpleUploadedFile(name='test_image.jpeg',
+                                                              content=open('/home/igoriusha/PycharmProjects/VirtualInternship/perevalapi/static/images/test_image.jpeg', 'rb').read(),
+                                                              content_type='image/jpeg'),
+                                    title='Test_image1')
+        self.assertEqual(str(pereval), f"Название: {pereval.beautyTitle} {pereval.title} {pereval.other_titles}")
